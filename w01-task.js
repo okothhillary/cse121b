9bdebb270a7c6a12a5621205ea4e249fa825b328
okothhillary <hobboltport@gmail.com>
2024-01-13 16:16:12 +0300

    debugging using browser dev tools

--- w01-task.js
+++ w01-task.js
@@ -14,6 +14,7 @@ let theTotal = total(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 console.log(`The total is ${theTotal}`);
 
 // 'total' function declaration
+
 function total(...theNumbers) {
 	let sum = 0;
 	for (let aNumber of theNumbers) {
